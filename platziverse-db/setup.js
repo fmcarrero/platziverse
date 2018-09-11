56a33d0754174b1df4703fd4b5157f4362216332
Franklin carrero <franklin.carrero@ceiba.com.co>
2018-09-11 15:59:45 -0500

    firts commit

---
+++ platziverse-db/setup.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const db = require('./')
+const debug = require('debug')('platziverse:db:setup')
+async function setup () {
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true
+  }
+  await db(config).catch(handleFatalEerror)
+  console.log('success')
+  process.exit(0)
+}
+function handleFatalEerror (err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
+}
+setup()
